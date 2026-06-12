d1b89e727633c714afe55e6bf3fe53bdceb3a51d
Emmanuel <Emmanuel@DESKTOP-45B5730>
2026-06-12 12:33:57 -0700

    completed part arrays and reporting

---
+++ QuickchatApp-master/src/main/java/Config.java
@@ -0,0 +1,8 @@
+
+
+public class Config {
+    public static final String APP_NAME = "QuickChat";
+    public static final String VERSION = "1.0";
+    public static final int MAX_MESSAGE_LENGTH = 250;
+    public static final int MIN_PASSWORD_LENGTH = 8;
+}
